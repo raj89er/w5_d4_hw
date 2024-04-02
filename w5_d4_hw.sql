eeba7410d7da354c05a644612cf0f13062b2d544
raj89er <134079776+raj89er@users.noreply.github.com>
2024-04-02 18:09:52 -0400

    corrected Q1

--- w5_d4_hw.sql
+++ w5_d4_hw.sql
@@ -12,13 +12,13 @@ CREATE OR REPLACE PROCEDURE InsertFilm(
     IN p_rental_rate DECIMAL(5, 2),
     IN p_length INTEGER,
     IN p_replace_cost DECIMAL(5, 2),
-    IN p_rating VARCHAR(10)
+    IN mpaa_rating mpaa_rating
 )
 LANGUAGE plpgsql
 AS $$
 BEGIN
     INSERT INTO film (title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating, last_update)
-    VALUES (p_title, p_description, p_release_year, p_language_id, p_rental_duration, p_rental_rate, p_length, p_replace_cost, p_rating, CURRENT_TIMESTAMP);
+    VALUES (p_title, p_description, p_release_year, p_language_id, p_rental_duration, p_rental_rate, p_length, p_replace_cost, mpaa_rating, CURRENT_TIMESTAMP);
 END;
 $$;
 
@@ -34,7 +34,9 @@ CALL InsertFilm(
 	'PG-13'
 );
 
-
+SELECT *
+FROM film
+WHERE title ILIKE "cloud atlas";
 
 -- 2. Create a Stored Function that will take in a category_id and return the number of films in that category
 
